ae081a461c2daa4ece6aff8bfb9edf9872b00e35
Albion Bame <albionbame@gmail.com>
2022-12-10 18:47:15 +0100

    updated preview to match the new content type

--- assets/js/preview.js
+++ assets/js/preview.js
@@ -11,17 +11,52 @@ $(async function () {
     if(article.length !== 1) {
         return;
     }
-    console.log(article);
+
+    const groupedQuestionsAnswers = article[0].fields.questionsAnswers !== undefined ? article[0].fields.questionsAnswers.reduce(
+        (result, item) => ({
+        ...result,
+        [item["group"]]: [
+            ...(result[item["group"]] || []),
+            item,
+        ],
+        }), 
+        {},
+    ) : [];
 
     //visualise article details
     const createdAt = new Date(article[0].sys.createdAt);
     const month = createdAt.toLocaleString('default', { month: 'long' });
-    const headerBackgroundImageId = article[0].fields.headerBackgroundImage.sys.id;
+    const headerBackgroundImageId = article[0].fields.headerBackgroundImage?.sys.id;
     $("div.page-heading > h1").html(article[0].fields.title);
     $("div.page-heading").append(`<span class="meta">Posted on ${month} ${createdAt.getDate()}, ${createdAt.getFullYear()}</span>`);
-    $('article.post-preview').html(documentToHtmlString(article[0].fields.description));
+    let html = "";
+    if(article[0].fields.description !== undefined) {
+        html += await documentToHtmlString(article[0].fields.description);
+    }
+
+    for (const group in groupedQuestionsAnswers) {
+        html += `<div id="accordion"><div class="head"><h2>${group}</h2><i class="fas fa-angle-down arrow"></i></div>`;
+        html += `<div class="content">`;
+        for (const [index, questionAnswer] of groupedQuestionsAnswers[group].entries()) {
+            html += `<p style="font-weight: bold;">Exercise ${index + 1}: </p>`;
+            html += `<p>${questionAnswer.question}</p>`;
+            //console.log(questionAnswer.answer);
+        }
+        html += "</div></div>";
+    }
+    $('article.post-preview').html(html);
 
     //get and visualise background image
-    const headerBackgroundImage = await $.get(`https://preview.contentful.com/spaces/${params.get('spaceId')}/environments/master/assets/${headerBackgroundImageId}?access_token=${params.get('token')}`);
-    $('header.masthead').css('background', `url('${headerBackgroundImage.fields.file.url.replace('//', 'https://')}') 0% 0% / 100% 100% no-repeat`);
+    if (headerBackgroundImageId) {
+        const headerBackgroundImage = await $.get(`https://preview.contentful.com/spaces/${params.get('spaceId')}/environments/master/assets/${headerBackgroundImageId}?access_token=${params.get('token')}`);
+        $('header.masthead').css('background', `url('${headerBackgroundImage.fields.file.url.replace('//', 'https://')}') 0% 0% / 100% 100% no-repeat`);
+    }
+});
+
+$(document).on("click", "div.head" , function() {
+    $("div.head").removeClass('active');
+    $("div.content").css('display', 'none');
+    $(this).toggleClass('active');
+    $(this).parent().find('.arrow').toggleClass('arrow-animate');
+    $(this).parent().find('.content').slideToggle(280);
 });
