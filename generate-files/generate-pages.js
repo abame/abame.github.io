6b2931a6148403faddeb1e64217d59282568f35f
Albion Bame <abame@users.noreply.github.com>
2022-12-13 06:11:04 +0100

    Update page generation (#6)
    * updated gitignore with generic rules
    
    * updated workflow to use Node.js to build pages
    
    * added nodejs application to generate pages
    
    * env variables globaly in workflow
    
    * run workflow on all branches
    
    * exclude nodejs app from jekyll build

---
+++ generate-files/generate-pages.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const contentful = require('contentful');
+const nunjucks = require('nunjucks');
+const fs = require('fs');
+const richTextRenderer = require('@contentful/rich-text-html-renderer');
+
+// ============ SETUP CONFIGURATION START ===============
+let contentfulConfig = {
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_SECRET,
+    environment: 'master',
+};
+
+if(process.argv[3] === 'preview') {
+    contentfulConfig = { ...contentfulConfig, ...{ host: 'preview.contentful.com', accessToken: process.env.PREVIEW_CONTENTFUL_SECRET }}
+}
+
+const contentType = process.argv[2];
+let directory = contentType === "blogPost" ? "posts" : undefined;
+const defaultImage = '/img/bg-post.jpeg';
+
+nunjucks.configure('templates', { autoescape: false });
+
+const client = contentful.createClient(contentfulConfig);
+// ============ SETUP CONFIGURATION END ===============
+
+const camalize = (str) => {
+    return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+}
+
+const kebabCase = string => string
+.replace(/([a-z])([A-Z])/g, "$1-$2")
+.replace(/[\s_]+/g, '-')
+.toLowerCase();
+
+client.getEntries({order: 'sys.createdAt', content_type: contentType})
+    .then((entries) => {
+        for (const entry of entries.items) {
+            directory = entry.fields.learningType !== undefined ? camalize(entry.fields.learningType) : directory
+            const groupedQuestionsAnswers = entry.fields.questionsAnswers !== undefined ? entry.fields.questionsAnswers.reduce(
+                (result, item) => ({
+                ...result,
+                [item["group"]]: [{...item, answer: item.answer.replace(/(?:\r\n|\r|\n)/g, '<br>')}],
+                }), 
+                {},
+            ) : [];
+            const image = entry.fields.headerBackgroundImage !== undefined ? `https:${entry.fields.headerBackgroundImage.fields.file.url}` : defaultImage;
+            const html = nunjucks.render(`${kebabCase(contentType)}.html`, { 
+                title: entry.fields.title,
+                date: entry.sys.createdAt,
+                backgroundImage: image,
+                description: richTextRenderer.documentToHtmlString(entry.fields.description ?? ""),
+                questionsAnswers: groupedQuestionsAnswers,
+            });
+            fs.writeFileSync(`../_${directory}/${entry.sys.createdAt.split('T')[0]}-${camalize(entry.fields.title)}.html`, html);
+        }
+    });
