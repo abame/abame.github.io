e606b4af524d5af10459facd01c4cd2c0e345655
Albion Bame <abame@users.noreply.github.com>
2022-12-26 15:20:10 +0100

    Feature learning paths (#10)
    Added learning path functionality

---
+++ generate-files/helpers.js
@@ -0,0 +1,26 @@
+const kebabCase = string => string.replace(/([a-z])([A-Z])/g, "$1-$2").replace(/[\s_]+/g, '-').toLowerCase();
+const camalize = (str) => {
+    return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+}
+
+const groupByProperty = (arrayOfObjects = [], groupBy = "") => {
+    return arrayOfObjects.reduce((result, item) => {
+        if (item[groupBy] !== undefined) {
+            const groupByProperty = item[groupBy];
+            result[groupByProperty] = result[groupByProperty] ? result[groupByProperty] : [];
+            result[groupByProperty].push(item);
+            return result;
+        }
+      }, {});
+};
+
+const capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+module.exports = {
+    toKebabCase: kebabCase,
+    toCamelCase: camalize,
+    groupByProperty,
+    capitalize
+}
