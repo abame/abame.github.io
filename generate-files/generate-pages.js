9ca68356d274e8ce54c075f60e00112a31828a5c
Albion Bame <albionbame@gmail.com>
2023-04-16 11:22:33 +0200

    Add meta keywords and enable menu
    Added meta keywords for SEO in learning path articles and enabled the
    learning path navigation menu

--- generate-files/generate-pages.js
+++ generate-files/generate-pages.js
@@ -7,6 +7,11 @@ const contentType = process.argv[2];
 let directory = contentType === "blogPost" ? "posts" : undefined;
 const defaultImage = '/img/bg-post.webp';
 
+const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  }
+
 config.client.getEntries({order: 'sys.createdAt', content_type: contentType})
     .then((entries) => {
         for (const entry of entries.items) {
@@ -14,7 +19,7 @@ config.client.getEntries({order: 'sys.createdAt', content_type: contentType})
             const groupedQuestionsAnswers = entry.fields.questionsAnswers !== undefined ? helpers.groupByProperty(entry.fields.questionsAnswers, "group") : [];
             let image = defaultImage;
             if (entry.fields.headerBackgroundImage !== undefined) {
-                image = `https:${entry.fields.headerBackgroundImage.fields.file.url}`;
+                image = `https:/${entry.fields.headerBackgroundImage.fields.file.url}`;
             }
             const html = config.templateRenderer.render(`${helpers.toKebabCase(contentType)}.html`, { 
                 title: entry.fields.title,
@@ -22,6 +27,7 @@ config.client.getEntries({order: 'sys.createdAt', content_type: contentType})
                 backgroundImage: image,
                 description: richTextRenderer.documentToHtmlString(entry.fields.description ?? ""),
                 questionsAnswers: groupedQuestionsAnswers,
+                tags: entry.metadata.tags.map((tag) => capitalize(tag.sys.id.replace(/([a-z0-9])([A-Z])/g, '$1 $2'))).join(', ')
             });
             fse.outputFileSync(`../_${directory}/${entry.sys.createdAt.split('T')[0]}-${helpers.toKebabCase(entry.fields.title)}.html`, html);
         }
