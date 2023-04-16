274ce7acd0f48fb254993e266e6f6936cdcd9f4b
Albion Bame <albionbame@gmail.com>
2023-04-16 19:04:25 +0200

    Learning path meta description
    Updated meta description with a more meaningful text for better SEO

--- generate-files/generate-pages.js
+++ generate-files/generate-pages.js
@@ -3,6 +3,7 @@ import { toCamelCase, groupByProperty, toKebabCase } from './helpers.js';
 import fsExtra from 'fs-extra';
 import moment from "moment";
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { stripHtml } from "string-strip-html";
 
 const { outputFileSync } = fsExtra;
 
@@ -24,11 +25,13 @@ client.getEntries({order: 'sys.createdAt', content_type: contentType})
             if (entry.fields.headerBackgroundImage !== undefined) {
                 image = `https:${entry.fields.headerBackgroundImage.fields.file.url}`;
             }
+            const description = documentToHtmlString(entry.fields.description ?? "");
             const html = templateRenderer.render(`${toKebabCase(contentType)}.html`, { 
                 title: entry.fields.title,
                 date: moment(entry.sys.createdAt).format('YYYY-MM-DDTHH:mm:ssZZ'),
                 backgroundImage: image,
-                description: documentToHtmlString(entry.fields.description ?? ""),
+                description: description,
+                metaDescription: stripHtml(description).result.substring(0, 160),
                 questionsAnswers: groupedQuestionsAnswers,
                 tags: entry.metadata.tags.map((tag) => capitalize(tag.sys.id.replace(/([a-z0-9])([A-Z])/g, '$1 $2'))).join(', ')
             });
